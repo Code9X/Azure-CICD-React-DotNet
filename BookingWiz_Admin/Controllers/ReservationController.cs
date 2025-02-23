d47621398dcc1a1c4a8657a63bfdf52fd642c9b0
Code9X <sebinsaji0809@gmail.com>
2025-02-23 21:32:12 +0530

    Modified Reservation API

---
+++ BookingWiz_Admin/Controllers/ReservationController.cs
@@ -0,0 +1,21 @@
+﻿using BookingWiz_Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingWiz_Admin.Controllers
+{
+    [Route("/api/reservation/")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    { 
+        [HttpGet("list")]
+        public IActionResult GetProducts()
+        { 
+            var reservation = new List<Reservation>
+            { 
+                new Reservation { Id = 1, Name = "Sebin", Address="Test1", Location = "India" },
+                new Reservation { Id = 1, Name = "Steev", Address="Test2", Location = "Germany" }
+            };
+            return Ok(reservation);
+        }
+    }
+}
---
+++ BookingWiz_Admin/Models/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingWiz_Admin.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+    }
+}
